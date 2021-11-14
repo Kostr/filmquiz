fcadf3041bcbb532291de28759944b6d8f35dd83
Konstantin Aladyshev <aladyshev22@gmail.com>
2021-11-14 13:51:45 +0300

    Add script to generate html/js files from developer console dict dump
    Signed-off-by: Konstantin Aladyshev <aladyshev22@gmail.com>

---
+++ transform_data.py
@@ -0,0 +1,16 @@
+import codecs
+
+d = {}
+with codecs.open("181.txt", 'r', encoding='utf-8') as dict_file:
+    for line in dict_file:
+        d[line[0:103]] = line[106:-2]
+
+with codecs.open("181.html", 'w', encoding='utf-8') as html_file:
+    for k, v in d.items():
+        html_file.write(u'<h3>{name}</h3><img src="{link}">\n'.format(name=v, link=k))
+
+with codecs.open("181.js", 'w', encoding='utf-8') as js_file:
+    js_file.write(u'data = {\n')
+    for k, v in d.items():
+        js_file.write(u'"{link}" : "{name}"\n'.format(name=v, link=k))
+    js_file.write(u'};\n')
